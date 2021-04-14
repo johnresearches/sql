ee2fbdbf89db4042b2e3ef51f04df6dc58a9158e
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-04-13 23:55:34 -0400

    Update nested_select_queries.sql

--- competitive/hr/nested_select_queries.sql
+++ competitive/hr/nested_select_queries.sql
@@ -2,11 +2,18 @@
 Enter your query here.
 SELECT COUNT(CustomerID), Country
 FROM Customers
+
+
 GROUP BY Country
+
 HAVING COUNT(CustomerID) > 5
+
 ORDER BY COUNT(CustomerID) DESC;
+
 select MIN(CHARACTER_LENGTH(CITY)) from STATION;
+
 select MAX(CHARACTER_LENGTH(CITY)) from STATION;*/
+
 SELECT CITY, (select MIN(CHARACTER_LENGTH(CITY)) from STATION)
 FROM STATION
 GROUP BY CITY
