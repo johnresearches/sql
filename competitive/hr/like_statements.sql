aaa487b845ec7f205b5c3eb1795a3d45b6a103de
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-07-20 20:51:31 -0400

    Update like_statements.sql

--- competitive/hr/like_statements.sql
+++ competitive/hr/like_statements.sql
@@ -1,10 +1,7 @@
 /*
 Enter your query here.
-
-SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '[aeiou]%'; (didn't work but it should)
-*/
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '[aeiou]%'; (didn't work but it should)*/
 SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE 'a%';
-
 
 SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE 'e%';
 
