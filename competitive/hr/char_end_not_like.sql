119fbb26cd26ca180b034aae256ec79435f20bb8
johnresearches <76896708+johnresearches@users.noreply.github.com>
2021-10-07 20:59:10 -0400

    Update char_end_not_like.sql

--- competitive/hr/char_end_not_like.sql
+++ competitive/hr/char_end_not_like.sql
@@ -1,5 +1,5 @@
 /*
-Enter your query heres
+Ener your query heres
 */
 
 SELECT DISTINCT CITY FROM STATION WHERE CITY NOT LIKE '%a' AND CITY NOT LIKE '%e' AND CITY NOT LIKE '%i' AND CITY NOT LIKE '%o' AND CITY NOT LIKE '%u';
